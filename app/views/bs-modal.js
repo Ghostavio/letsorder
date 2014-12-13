8d3342a38a0b44d35c1ca70ced39bef1cfd333ee
Tor Sanden <tor@intrisis.com>
2014-12-13 14:25:14 +0400

    logo navbar

--- app/views/bs-modal.js
+++ app/views/bs-modal.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.View.extend({
   tagName: 'buttons',
   classNames: ['btn', 'btn-lg', 'btn-danger'],
-  attributeBindings: ['title'],
+  attributeBindings: ['title', 'content'],
   didInsertElement: function() {
     this.$().modal('show');
   }
