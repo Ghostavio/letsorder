dd8b03cd3ded3c00f20861e22b45502774b2068f
Gustavo Siqueira <gustavo.anacleto@gmail.com>
2014-12-13 14:10:15 +0400

    timer page with link to next page

--- app/routes/group/timer.js
+++ app/routes/group/timer.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    // var userID = this.get('session').content.userID;
+    return this.store.find('restaurant', params.restaurant_id);
+  }
 });
