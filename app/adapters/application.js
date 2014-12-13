2fe50a082b156565c97924ba40a69a5ddc96f23f
Gustavo Siqueira <gustavo.anacleto@gmail.com>
2014-12-13 14:40:59 +0400

    doing items selectable

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,7 +4,3 @@ import config from '../config/environment';
 export default DS.FirebaseAdapter.extend({
   firebase: new window.Firebase('https://' + config.modulePrefix + '.firebaseio.com')
 });
-
-// export default DS.ActiveModelAdapter.extend({
-//   host: config.apiUrl
-// });
---
+++ app/views/item-select.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+  tagName: 'li',
+  classNames: ['friend-name no-style'],
+  click: function() {
+    var selectedItems = this.get('controller').get('selectedItems'),
+        item = this.get('item');
+    if(!this.$().hasClass('selected-friend')) {
+        selectedItems.pushObject(item);
+        this.$().addClass('selected-friend');
+      } else {
+        selectedItems.popObject(item);
+        this.$().removeClass('selected-friend');
+      }
+  }
+});
