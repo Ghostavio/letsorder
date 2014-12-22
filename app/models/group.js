37266ef1a60c6e75096e5eb0b301530002dd324b
Tor Sanden <tor@intrisis.com>
2014-12-22 16:57:43 +0400

    timer in navbar

--- app/models/group.js
+++ app/models/group.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name        : DS.attr('string'),
   picture     : DS.attr('string'),
+  temporary : DS.attr('boolean'),
   members     : DS.hasMany("user",       { async: true }),
   restaurants : DS.hasMany("restaurant", { async: true }),
   user        : DS.belongsTo("user",     { async: true }),
