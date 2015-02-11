66235ffc1f3f862884ffaae854153f09bb879239
Gustavo Siqueira <gustavo.anacleto@gmail.com>
2015-02-11 20:36:26 +0400

    added error handling in the setup page in case username is empty

--- app/controllers/setup.js
+++ app/controllers/setup.js
@@ -1,14 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  errorMsg: null,
+  displayErrorMsg: false,
   actions: {
     setupUser: function() {
       var self = this,
           currentUser = this.get('model');
-      currentUser.setProperties({ username: this.get('content.username'), phone: this.get('content.phone') });
-      currentUser.save().then(function(){
-        self.transitionToRoute('group');
-      });
+      if(!this.get('content.username')) {
+        this.set('errorMsg', 'Please enter your username.');
+        this.set('displayErrorMsg', true);
+      } else {
+        currentUser.setProperties({ username: this.get('content.username'), phone: this.get('content.phone') });
+        currentUser.save().then(function(){
+          self.transitionToRoute('group');
+          self.set('displayErrorMsg', false);
+        });
+      }
     }
   }
 });
