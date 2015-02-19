2cf8d18b5383b4090557bd9a932bac20a8105826
graphmeter <graphmeter@gmail.com>
2015-02-19 18:12:26 +0100

    add settings page and route

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+	model: function() {
+    var userID = this.get('session').content.userID;
+    return this.store.find('user', userID);
+  }
 });
