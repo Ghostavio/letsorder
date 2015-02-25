0f9da6dee4386078029952ef1395145f25fbd717
Tor Sanden <tor@intrisis.com>
2015-02-25 21:17:15 +0100

    create group from selection

--- app/controllers/group.js
+++ app/controllers/group.js
@@ -1,10 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  selectedFriends: [],
-  actions: {
-    selectFriend: function(friend) {
-      console.log('SELECT', friend);
-    }
-  }
+  selectedFriends: []
 });
